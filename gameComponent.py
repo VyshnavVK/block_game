1e2631765ac6b64d09ff4187bdaabee485ce9605
vyshnavk <vyshnavvyshu20@gmail.com>
2024-07-03 00:53:01 +0530

    game component added, also optimized the game

---
+++ gameComponent.py
@@ -0,0 +1,57 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+coinIdSet = {0}
+score = 0
+
+
+def coins(width, height, padding, coinId):
+    if coinId in coinIdSet:
+        return
+    coin_img = pygame.image.load("assets/coin.png")
+    coin_rect = coin_img.get_rect()
+    coin_rect.topleft = (width, height - padding)
+    return coin_img, coin_rect, coinId
+
+
+def updateScoreCount(coinId: int = 0, resetScore: bool = False):
+    if resetScore:
+        coinIdSet.clear()
+        coinIdSet.add(0)
+
+    global score
+    if coinId in coinIdSet:
+        score = (len(coinIdSet) - 1) * 100
+    else:
+        if coinId != 0:
+            coinIdSet.add(coinId)
+
+    font = pygame.font.Font(None, 70)
+    text = font.render(f"Score: {score}", True, WHITE)
+    text_rect = text.get_rect()
+    text_rect.topleft = (20, 30)
+    return text, text_rect
+
+
+def bomb(width, height, padding):
+    moving_obj_img = pygame.image.load("assets/bomb.png")
+    moving_obj_rect = moving_obj_img.get_rect()
+    moving_obj_rect.topleft = (width, height - padding)
+    return moving_obj_img, moving_obj_rect
+
+
+def game_over_check(width, height, game_over, window):
+    if game_over:
+        font = pygame.font.Font(None, 120)
+        font1 = pygame.font.Font(None, 50)
+        game_over_text = font.render("Game Over", True, (102, 43, 40))
+        game_press_enter_to_continue = font1.render("Press Enter to restart", True, (102, 43, 40))
+        window.blit(game_over_text,
+                    (width // 2 - game_over_text.get_width() // 2, height // 3 - game_over_text.get_height() // 2))
+        window.blit(game_press_enter_to_continue, (width // 2 - game_press_enter_to_continue.get_width() // 2,
+                                                   (height // 3) + 50 - game_press_enter_to_continue.get_height() // 2))
+
+
+class GameComponent:
+    pass
--- jump.py
+++ jump.py
@@ -1,4 +1,7 @@
+from enum import Enum
+
 import pygame
+from gameComponent import coins, updateScoreCount, bomb as b, game_over_check
 
 pygame.init()
 
@@ -10,10 +13,16 @@ clock = pygame.time.Clock()
 
 rect = pygame.Rect(135, SCREEN_HEIGHT - 137, 30, 30)
 vel = 10
+jumpMax = 20
+
 jump = False
 jumpCount = 0
-jumpMax = 20
 transparent_surface_start_width = 50
+moving_obj_vel = 1
+
+move_right = True
+game_over = False
+run = True
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -23,29 +32,43 @@ GREEN = (0, 255, 0)
 
 background = pygame.image.load("assets/bg.png")
 
+initial_character_position = (135, SCREEN_HEIGHT - 230)
 character = pygame.image.load("assets/character/man_standing.png")
 character_rect = character.get_rect()
-character_rect.topleft = (135, SCREEN_HEIGHT - 230)
+character_rect.topleft = initial_character_position
 
 platform_rect = pygame.Rect(0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100)
 
-moving_obj_img = pygame.image.load("assets/bomb.png")
-moving_obj_rect = moving_obj_img.get_rect()
-moving_obj_rect.topleft = (600, SCREEN_HEIGHT - 165)
-moving_obj_vel = 1
-move_right = True
-game_over = False
-run = True
+bomb = b(600, SCREEN_HEIGHT, 165)
+moving_obj_img = bomb[0]
+moving_obj_rect = bomb[1]
+
+bomb1 = b(300, SCREEN_HEIGHT, 350)
+moving_obj_img1 = bomb1[0]
+moving_obj_rect1 = bomb1[1]
+
 back_buffer = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+coin = coins(850, SCREEN_HEIGHT, 265, 1)
 
-def game_over_check():
-    if game_over:
-        font = pygame.font.Font(None, 120)
-        game_over_text = font.render("Game Over", True, (102, 43, 40))
-        window.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 3 - game_over_text.get_height() // 2))
-        pygame.display.flip()
-        pygame.time.wait(3*1000)
+updateScoreCount: updateScoreCount
+
+
+def reset_game():
+    global move_right, game_over, run, jump, jumpCount, character_rect
+    updateScoreCount(0, True)
+    move_right = True
+    game_over = False
+    run = True
+    jump = False
+    jumpCount = 0
+    character_rect.topleft = initial_character_position
+
+
+class Directions(Enum):
+    LEFT = 1
+    RIGHT = 2
+    JUMP = 3
 
 
 while run:
@@ -55,14 +78,21 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
-            if not jump and event.key == pygame.K_UP:
+            if not jump and (event.key == pygame.K_UP or Directions.JUMP == 0):
                 jump = True
                 jumpCount = jumpMax
+            if game_over and event.key == pygame.K_RETURN:
+                reset_game()
 
     keys = pygame.key.get_pressed()
-
+    left, right = False, False
     if not game_over:
-        move_x = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
+        if keys == pygame.K_LEFT or Directions.LEFT:
+            left = keys[pygame.K_LEFT]
+        if keys == pygame.K_RIGHT or Directions.RIGHT:
+            right = keys[pygame.K_RIGHT]
+
+        move_x = (right - left) * vel
         new_centerx = character_rect.centerx + move_x
 
         if transparent_surface_start_width < new_centerx < SCREEN_WIDTH:
@@ -83,30 +113,43 @@ while run:
 
         if move_right:
             moving_obj_rect.x += moving_obj_vel
+            moving_obj_rect1.x += moving_obj_vel
             if moving_obj_rect.x >= 650:
                 move_right = False
         else:
             moving_obj_rect.x -= moving_obj_vel
+            moving_obj_rect1.x -= moving_obj_vel
             if moving_obj_rect.x <= 600:
                 move_right = True
 
-        if character_rect.colliderect(moving_obj_rect):
+        if character_rect.colliderect(moving_obj_rect) or character_rect.colliderect(moving_obj_rect1):
             game_over = True
 
     window.blit(background, (0, 0))
 
     transparent_surface_bottom = pygame.Surface((SCREEN_WIDTH, 100), pygame.SRCALPHA)
-    transparent_surface_bottom.fill((64, 64, 64, 0))
+    transparent_surface_bottom.fill((0, 0, 0, 0))
     window.blit(transparent_surface_bottom, (0, SCREEN_HEIGHT - 100))
 
     transparent_surface_start = pygame.Surface((transparent_surface_start_width, SCREEN_HEIGHT), pygame.SRCALPHA)
-    transparent_surface_start.fill((64, 64, 64, 0))
+    transparent_surface_start.fill((0, 0, 0, 0))
     window.blit(transparent_surface_start, (0, 0))
-    window.blit(moving_obj_img, moving_obj_rect.topleft)
-    window.blit(character, character_rect.topleft)
-    pygame.display.flip()
 
-    game_over_check()
+    window.blit(moving_obj_img, moving_obj_rect.topleft)
+    window.blit(moving_obj_img1, moving_obj_rect1.topleft)
+
+    window.blit(character, character_rect.topleft)
+    window.blit(coin[0], coin[1])
+
+    updateScore = updateScoreCount()
+    if character_rect.colliderect(coin[1]):
+        updateScore = updateScoreCount(coin[2])
+
+    window.blit(updateScore[0], updateScore[1])
+
+    game_over_check(SCREEN_WIDTH, SCREEN_HEIGHT, game_over, window)
+
+    pygame.display.flip()
 
 pygame.quit()
 exit()
